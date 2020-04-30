60357067fd997eee488c7c76045176f13ed2a8f9
Dozier Atwell <dozier.atwell914@gmail.com>
2020-04-29 23:00:27 -0500

    Seconday wave of changes

--- app.js
+++ app.js
@@ -2,13 +2,34 @@
 var bodyParser  = require("body-parser"),
 mongoose        = require("mongoose"),
 express         = require("express"),
+http            = require("http"),
 app             = express();
+
+//LOCAL HOST TESTING
+// const http = require('http');
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.end('Hello World');
+// });
+
+// server.listen(port, hostname, () => {
+//     console.log(`Server running at http://${hostname}:${port}/`);
+// });
 
 //APP CONFIG
 mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+
+//HOSTNAME 
+const port = 3000;
+const hostname = '127.0.0.1';
 
 //MONGOOSE/MODEL CONFIG
 var blogSchema = new mongoose.Schema({
@@ -20,17 +41,22 @@ var blogSchema = new mongoose.Schema({
 });
 var Blog = mongoose.model("Blog", blogSchema);
 
-// RESTFUL ROUTES  
-
-
-
-// title
-// image 
-// body
-// created 
-
 // RESTFUL ROUTES 
 
-app.listen(process.env.PORT, process.env.IP, function() {
+app.get("/", function name(req, res) {
+    res.redirect("/blogs");
+});
+
+app.get("/blogs", function (req, res){
+    Blog.find({}, function(err, blogs){
+        if(err){
+            console.log("ERROR!");
+        } else {
+            res.render("index", {blogs: blogs});
+        }
+    });
+});
+
+app.listen(port, hostname, function() {
     console.log("SERVER IS RUNNING!");
 })
