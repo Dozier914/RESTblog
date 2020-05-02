5dfea87fe75530cbe1f52611bfd603507a9262cc
Dozier Atwell <dozier.atwell914@gmail.com>
2020-05-02 17:29:06 -0500

    Adding new blog post functionality

--- app.js
+++ app.js
@@ -5,22 +5,6 @@ express         = require("express"),
 http            = require("http"),
 app             = express();
 
-//LOCAL HOST TESTING
-// const http = require('http');
-
-// const hostname = '127.0.0.1';
-// const port = 3000;
-
-// const server = http.createServer((req, res) => {
-//     res.statusCode = 200;
-//     res.setHeader('Content-Type', 'text/plain');
-//     res.end('Hello World');
-// });
-
-// server.listen(port, hostname, () => {
-//     console.log(`Server running at http://${hostname}:${port}/`);
-// });
-
 //APP CONFIG
 mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
@@ -46,13 +30,41 @@ var Blog = mongoose.model("Blog", blogSchema);
 app.get("/", function name(req, res) {
     res.redirect("/blogs");
 });
-
+// INDEX ROUTE 
 app.get("/blogs", function (req, res){
     Blog.find({}, function(err, blogs){
         if(err){
             console.log("ERROR!");
         } else {
             res.render("index", {blogs: blogs});
+        }
+    });
+});
+
+// NEW ROUTE 
+app.get("/blogs/new", function(req, res) {
+    res.render("new");
+});
+// CREATE ROUTE 
+app.post("/blogs", function(req, res){
+    //create blog 
+    Blog.create(req.body.blog, function(err, newBlog){
+        if(err){
+            res.render("new");
+        } else {
+            //then, redirect to the index
+            res.redirect("/blogs");
+        }
+    });
+});
+
+// SHOW ROUTE 
+app.get("/blogs/:id", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.render("show", {blog: foundBlog});
         }
     });
 });
